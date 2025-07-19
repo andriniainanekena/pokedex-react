c6fd794f782776eac138d333a7d3d17367221fcc
Nekena Andriniaina <andriniainanekena@gmail.com>
2025-07-19 20:03:48 +0300

    feat: add base states for Pokémon data, pagination, search, and modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+    const [allPokemon, setAllPokemon] = useState([]);
+  const [filteredPokemon, setFilteredPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const itemsPerPage = 18;
+  
   return (
     <div className="pokedex-container">
       <header className="pokedex-header">
