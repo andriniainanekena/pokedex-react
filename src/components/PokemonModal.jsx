e5690087144f8bdf870b6acb0d2d9f4b187dceb1
Nekena Andriniaina <andriniainanekena@gmail.com>
2025-07-19 20:31:09 +0300

    feat: apply dynamic background color based on Pokémon primary type

--- src/components/PokemonModal.jsx
+++ src/components/PokemonModal.jsx
@@ -1,9 +1,36 @@
 import './PokemonModal.css';
 
 function PokemonModal({ pokemon, onClose }) {
+const typeColors = {
+  fire: '#f08030',
+  grass: '#78c850',
+  water: '#6890f0',
+  bug: '#a8b820',
+  normal: '#a8a878',
+  poison: '#a040a0',
+  electric: '#f8d030',
+  ground: '#e0c068',
+  fairy: '#ee99ac',
+  fighting: '#c03028',
+  psychic: '#f85888',
+  rock: '#b8a038',
+  ghost: '#705898',
+  ice: '#98d8d8',
+  dragon: '#7038f8',
+  dark: '#705848',
+  steel: '#b8b8d0',
+  flying: '#a890f0'
+};
+
+const primaryType = pokemon.types[0].type.name;
+const modalStyle = {
+  '--type-color': typeColors[primaryType] || '#a8a878',
+};
+
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+      <div className="modal-content" style={modalStyle} onClick={(e) => e.stopPropagation()}>
         <button className="modal-close" onClick={onClose}>✕</button>
 
         <div className="modal-pokemon-profile">
